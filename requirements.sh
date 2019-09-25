cfbb502bd487da73ab48d987e5f67541b0231952
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2019-09-25 15:38:40 +0200

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -10,7 +10,7 @@ set -e
 # pip3 install numpy==1.15.0
 # pip3 install seaborn==0.9.0
 # pip3 install bokeh==0.13.0
-pip3 install plotly
+pip3 install plotly==4.1.1
 
 
 # Include datasets in the image
